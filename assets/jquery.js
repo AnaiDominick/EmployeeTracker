762ebd678dc1ca1ca25a3b6c03311506aaa4754a
diegos21 <diego.sojo@udem.edu>
2018-11-17 10:48:13 -0600

    2

--- assets/jquery.js
+++ assets/jquery.js
@@ -1,15 +1,30 @@
 $( document ).ready(function() {
 
-    var clickCounter = 0;
+   
 
     $("#submit").click(function(event){
         event.preventDefault();
         clickCounter++;
-        var employeenme = $("#employeename").val().trim();
+        var employeename = $("#employeename").val().trim();
         var role = $("#role").val().trim();
         var startdate = $("#startdate").val().trim();
         var monthsworked =$("#monthsworked").val().trim();
 
+        database.ref("/employees").push({
+            name: employeename
+        });
+
+        database.ref("/role").push({
+            role: role
+        });
+
+        database.ref("/startdate").push({
+            startdate: startdate
+        });
+
+        database.ref("/monthsworked").push({
+            monthsworked: monthsworked
+        });
 
 
     });
